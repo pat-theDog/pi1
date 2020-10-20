82f56d465cc92f3711952b8bb49fadb011e19840
Pat <parth45@gmail.com>
2020-10-19 22:25:33 -0400

    Commit from pycharm

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def main(msg):
+	// Another comment
 	// Adding a different comment
 	// Print a message
 	print(msg)
 
 main("Hello People")
-
->>>>>>> refs/remotes/origin/main
